d585bcd0e9999645e33e62e819545403df06ddc6
WangShengqing122090536 <124855328+WangShengqing122090536@users.noreply.github.com>
2023-10-11 18:41:09 +0800

    commit

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
-import torch
 import data_utils
-from data_utils import *
+import random
 
 firstnamelist = data_utils.readList("materials/Raw materials/Raw_Firstname.txt")
 middlenamelist = data_utils.readList("materials/Raw materials/Raw_Middlename.txt")
 lastnamelist = data_utils.readList("materials/Raw materials/Raw_Lastname.txt")
 
-# print(firstnamelist)
-combined_names = [f"{first} {middle} {last}" for first in firstnamelist for middle in middlenamelist for last in
-                  lastnamelist]
-print(len(combined_names))
-print(combined_names)
+
+def get_N_names(N):
+    combined_names = [f"{first} {middle} {last}" for first in firstnamelist for middle in middlenamelist for last in
+                      lastnamelist]
+    selected_name_list = random.sample(combined_names, N)  # supposed to be 100000
+    return selected_name_list
+
+
+
+
+
+
+print(get_N_names(4))
